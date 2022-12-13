32eacf4112f9a1037d6805220eba7a6b686ebc39
Darpan Deb <86478640+darpandeb@users.noreply.github.com>
2022-12-13 14:26:07 +0530

    Update db.js

--- Loginlogout/db.js
+++ Loginlogout/db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://testuser:userpass@cluster0-shard-00-00.tu5oi.mongodb.net:27017,cluster0-shard-00-01.tu5oi.mongodb.net:27017,cluster0-shard-00-02.tu5oi.mongodb.net:27017/edureka?ssl=true&replicaSet=atlas-86s3em-shard-0&authSource=admin&retryWrites=true&w=majority")
+mongoose.connect("mongodb://testuser:testuser@cluster0-shard-00-00.tu5oi.mongodb.net:27017,cluster0-shard-00-01.tu5oi.mongodb.net:27017,cluster0-shard-00-02.tu5oi.mongodb.net:27017/edureka?ssl=true&replicaSet=atlas-86s3em-shard-0&authSource=admin&retryWrites=true&w=majority")
